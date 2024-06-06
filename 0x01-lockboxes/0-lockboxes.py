d7ddac56435450394b8bfc631a332a5a4a63ca7b
Taofeek Olusola <olusolataofeek@gmail.com>
2024-06-06 18:27:23 +0100

    0x01-lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,18 @@
+#!/usr/bin/python3
+"""
+Module to determine if all boxes can be opened.
+"""
+
 def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened.
+    
+    Args:
+        boxes (list of lists): A list where each index contains a list of keys.
+    
+    Returns:
+        bool: True if all boxes can be opened, else False.
+    """
     n = len(boxes)
     opened = set([0])
     keys = boxes[0][:]
@@ -8,5 +22,5 @@ def canUnlockAll(boxes):
         if key not in opened and key < n:
             opened.add(key)
             keys.extend(boxes[key])
-    
+
     return len(opened) == n
--- 0x01-lockboxes/README.md
+++ 0x01-lockboxes/README.md
@@ -1,1 +1,8 @@
-0x01-lockboxes
+# Can Unlock All Boxes
+
+This project contains a Python function to determine if all boxes in a given list can be unlocked. Each box may contain keys to other boxes, and the first box is always unlocked.
+
+## Function Prototype
+
+```python
+def canUnlockAll(boxes):
